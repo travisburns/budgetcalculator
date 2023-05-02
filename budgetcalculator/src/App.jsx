2637f6141a62f3a1d37b45738dd4a0d5bcb3c4d9
Travis <travisjb0@gmail.com>
2023-05-02 16:05:50 -0700

    added functionality for adding expenses to calculator using state

--- budgetcalculator/src/App.jsx
+++ budgetcalculator/src/App.jsx
@@ -14,22 +14,54 @@ const initialExpenses = [
 console.log(initialExpenses);
 
 function App() {
+  // **** state values
+  // all expenses, add expense
     const [expenses, setExpenses] = useState(initialExpenses);
-    
+    // ** single expense
 
+    const [charge, setCharge] = useState('');
+    // *** functionallity
+    const [amount, setAmount] = useState('');
+
+    const handleCharge = e => {
+      setCharge(e.target.value)
+    }
+
+    const handleAmount = e => {
+      setAmount(e.target.value)
+    }
+
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      if(charge !=='' && amount > 0) {
+        const singleExpense = {id:uuidv4(),charge, amount };
+        setExpenses([...expenses, singleExpense])
+        setCharge('');
+        setAmount('');
+      } else {
+        // handle alert called
+      }
+    }
 
   return <>
   <Alert />
   <h1>budget calculator</h1>
   <main className="App">
-  <ExpenseForm/>
+  <ExpenseForm 
+  charge={charge} 
+  amount={amount} 
+  handleAmount={handleAmount} 
+  handleCharge={handleCharge} 
+  handleSubmit={handleSubmit}
+  />
   <ExpenseList  expenses={expenses}/>
   </main>
   <h1>
     
       Total Spending : <span className='total'>
         $ {expenses.reduce((acc, curr)=>{
-          return (acc += curr.amount)
+          return (acc += parseInt (curr.amount));
         }, 0)}
       </span>
     </h1>
